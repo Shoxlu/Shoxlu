62c8f31cbf8c2b25a8972284f44bc6e42babb9bc
Shoxlu <shoxlu0712@gmail.com>
2022-04-28 18:25:04 +0200

    Revert "Revert "Initial commit""
    This reverts commit c949b732c4b1190f57185760b627a736c46bf215.

---
+++ ShoxBazar/patch.js
@@ -0,0 +1,9 @@
+{
+        "dependencies": [
+                {
+				"archive": "repos/nmlgc/base_tsa/",
+				"archive": "repos/exphp/bullet_cap/",
+				"archive": "repos/Shoxlu/ShoxBazar/"
+				}
+        ]
+}
