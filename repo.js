5cdfd79a4f73a3224b14aeff757b3c29af720908
Shoxlu <shoxlu0712@gmail.com>
2022-04-29 20:00:56 +0200

    Update repo.js

--- repo.js
+++ repo.js
@@ -3,6 +3,7 @@
 	"id": "Shoxlu",
 	"patches": {
 		"ShoxBazar": "idk, tests(don't play that plz)"
+		"ModCdI": "My first mod ever,for th13 (unfinished, probably) based on some people on the Café des Illusions discord server",
 	},
 	"servers": [
 		"https://mirrors.thpatch.net/Shoxlu/"
