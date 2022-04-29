e054cb1285e2882e51f69bf97ab9c55ec5548472
Shoxlu <shoxlu0712@gmail.com>
2022-04-29 19:57:46 +0200

    yes

--- ShoxBazar/patch.js
+++ ShoxBazar/patch.js
@@ -1,4 +1,9 @@
 {
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"Shoxlu/ShoxBazar",
+		"repos/exphp/bullet_cap/"
+	],
     "id": "ShoxBazar",
     "servers": [
         "https://mirrors.thpatch.net/Shoxlu/ShoxBazar"
