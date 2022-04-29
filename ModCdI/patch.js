1b851741e5dde84ea62a1b298288a21e1716e085
Shoxlu <shoxlu0712@gmail.com>
2022-04-29 20:28:54 +0200

    Update patch.js

--- ModCdI/patch.js
+++ ModCdI/patch.js
@@ -1,16 +1,7 @@
 {
-	"dependencies": [
-      "archive": "repos/nmlgc/base_tsa/",
-      "archive": "repos/nmlgc/script_latin/",
-      "archive": "repos/thpatch/lang_fr/",
-      "archive": "repos/ExpHP/base_exphp/",
-      "archive": "repos/ExpHP/anm_leak/",
-      "archive": "repos/ExpHP/sprite_death_fix/",
-      "archive": "repos/ExpHP/bullet_cap/"
-	],
-    "id": "ModCdI",
+    "id": "ModCdi",
     "servers": [
-        "https://mirrors.thpatch.net/Shoxlu/ModCdI"
+        "https://mirrors.thpatch.net/Shoxlu/ModCdi"
     ],
     "title": "My first mod ever,for th13 (unfinished, probably) based on some people on the Café des Illusions discord server"
 }
