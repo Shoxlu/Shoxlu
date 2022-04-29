50875c55a2f997d339a42226cfadf3fb9f7f44b4
Shoxlu <shoxlu0712@gmail.com>
2022-04-29 21:16:45 +0200

    Update patch.js

--- ModCdI/patch.js
+++ ModCdI/patch.js
@@ -1,7 +1,7 @@
 {
     "id": "ModCdi",
     "servers": [
-        "https://mirrors.thpatch.net/Shoxlu/ModCdi"
+        "https://mirrors.thpatch.net/Shoxlu/ModCdI"
     ],
     "title": "My first mod ever,for th13 (unfinished, probably) based on some people on the Café des Illusions discord server"
 }
