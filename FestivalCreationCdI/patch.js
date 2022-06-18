d68a8ff8c17d6b3c20c62c63e4ee09989b2ef4a3
Shoxlu <shoxlu0712@gmail.com>
2022-06-18 22:07:46 +0200

    Update patch.js

--- FestivalCreationCdI/patch.js
+++ FestivalCreationCdI/patch.js
@@ -7,5 +7,5 @@
     "servers": [
         "https://mirrors.thpatch.net/Shoxlu/FestivalCreationCdI"
     ],
-    "title": "Un mod créé à l'occasion du Festival de Créations du serveur francophone \"Le Café des Illusions\" sur le thème de Touhou 7 et du printemps !"
+    "title": "Un mod créé avec Lance#7161(Sprites, dialogues et autres) et MLM3#1370(Musique) à l'occasion du Festival de Créations du serveur francophone \"Le Café des Illusions\" sur le thème de Touhou 7 et du printemps !"
 }
