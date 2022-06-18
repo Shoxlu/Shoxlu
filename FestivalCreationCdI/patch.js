e038997c690b5fa3578faa698f31c0bbe2605ef5
Shoxlu <shoxlu0712@gmail.com>
2022-06-18 22:01:37 +0200

    Add FestivalCréa on thcrap

---
+++ FestivalCreationCdI/patch.js
@@ -0,0 +1,11 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"repos/exphp/bullet_cap/"
+	],
+    "id": "FestivalCreaCdI",
+    "servers": [
+        "https://mirrors.thpatch.net/Shoxlu/FestivalCreationCdI"
+    ],
+    "title": "Un mod créé à l'occasion du Festival de Créations du serveur francophone \"Le Café des Illusions\" sur le thème de Touhou 7 et du printemps !"
+}
