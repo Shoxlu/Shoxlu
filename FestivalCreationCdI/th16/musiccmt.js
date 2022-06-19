5b262ebe0c73ab6a9d9a55a48a92db5cf7221582
Shoxlu <shoxlu0712@gmail.com>
2022-06-19 19:53:43 +0200

    add FestivalCreation files

---
+++ FestivalCreationCdI/th16/musiccmt.js
@@ -0,0 +1,160 @@
+{
+    "1": [
+        "@",
+        "",
+        "C'est le thème de l'écran titre.",
+        "",
+        "Un thème avec un sentiment du style «Nous allons bientôt commencer!».",
+        "Il a fini par avoir un style japonais, léger, et un peu sérieux."
+    ],
+    "2": [
+        "@",
+        "",
+        "C'est le thème du niveau 1.",
+        "",
+        "J'ai essayé d'amener un sentiment d'«introduction» et l'exaltation de l'été.",
+        "C'est un thème plutôt léger, même pour les standards de Touhou. Je me demande si j'ai réussi à faire ressentir une sensation de vitesse.",
+        "Cela dit, ça va bien avec de la bière."
+    ],
+    "3": [
+        "@",
+        "",
+        "C'est le thème d'Eternity Larva.",
+        "",
+        "Je suis allé chercher la sensation d'une fée un peu mystérieuse.",
+        "Je pense que les papillons qui apparaissent dans les jeux vidéos sont certainement un symbole mystérieux.",
+        "Je veux dire, c'est juste une fée donc sa tête est vide, mais bon."
+    ],
+    "4": [
+        "@",
+        "",
+        "C'est le thème du niveau 2.",
+        "",
+        "En contraste avec la joie interminable du niveau 1, j'ai ajouté une sorte de mélancolie à celle-ci",
+        "et j'ai ramené également une sensation automnale.",
+        "Adapter la musique pour qu'elle soit uniforme à travers le jeu, même quand les saisons changent,",
+        "est plutôt amusant."
+    ],
+    "5": [
+        "@",
+        "",
+        "C'est le thème de Nemuno Sakata.",
+        "",
+        "Au final, je pense que ce thème fait très jeu vidéo.",
+        "Et puis, c'est une yamanba. Elles apparaissent souvent dans les histoires populaires, mais malgré cela",
+        "ce sont des créatures assez difficiles à cerner.",
+        "En utilisant le frisson et la ferveur de sang chaud comme ma base, j'en ai fait un thème dans le style de Touhou."
+    ],
+    "6": [
+        "@",
+        "",
+        "C'est le thème du niveau 3.",
+        "",
+        "Le concept de celui-ci est de voler haut dans le ciel de la nuit,",
+        "où seules les fleurs de cerisier brillent dans la lumière nocturne.",
+        "",
+        "Ces fleurs ont toujours eu un sentiment de fugacité à leurs propos, et cette fois-ci ne fait pas exception."
+    ],
+    "7": [
+        "@",
+        "",
+        "C'est le thème d'Aunn Komano.",
+        "",
+        "Un sentiment de printemps, de bravoure, celui d'un adversaire plutôt fort, ainsi que de celui d'une mascotte.",
+        "C'est un thème un peu fourre-tout avec toutes ces choses entassées dedans.",
+        "Son concept de personnage est «une seule bête divine fonctionnant comme une paire», mais elle n'a pas",
+        "de double personnalité ou autre chose. Aussi, vous ne pouvez pas le voir, mais elle a une queue !"
+    ],
+    "8": [
+        "@",
+        "",
+        "C'est le thème du niveau 4.",
+        "",
+        "Quand on pense à l'hiver, on pense au blizzard.",
+        "Vous ne savez peut-être pas s'il ne neige pas souvent là où vous habitez, mais les blizzards",
+        "sont différents des tempêtes. Ils se produisent quand la neige se fait balayer du sol et redescend",
+        "en pente. Porter un chapeau de paille pendant un blizzard ne sert à rien."
+    ],
+    "9": [
+        "@",
+        "",
+        "C'est le thème de Narumi Yatadera.",
+        "",
+        "J'ai composé cette musique avec en tête l'image d'une personne qui a ses pieds fermement plantés dans le sol. ",
+        "Le Kṣitigarbha Bodhisattva original est une figure honorable qui donnait le salut aux croyants avant l'apparition de Maitreya, mais à un moment ou un autre, un nombre incroyable de statues Kṣitigarbha se sont répandues dans le Japon. Donc je me suis dit que ce serait plutôt sympathique s'il y avait une statue Kṣitigarbha quelque part là-dedans."
+    ],
+    "10": [
+        "@",
+        "",
+        "C'est le thème du niveau 5.",
+        "",
+        "Le monde le plus angoissant vu dans Touhou jusqu'à présent.",
+        "Même si, quand on considère le fait que toutes ces portes sont liées au dos des gens, je suppose qu'on pourrait quand même trouver ça pratique.",
+        "Ces portes ont l'air d'être peu enthousiastes."
+    ],
+    "11": [
+        "@",
+        "",
+        "C'est le thème de Mai Teireida et de Satono Nishida.",
+        "",
+        "J'ai incorporé beaucoup de choses dans cette musique: étrangeté, légèreté,",
+        "forte hostilité, innocence, et pour couronner le tout, un sentiment rappelant un mid-boss.",
+        "J'ai mis assez pour les deux personnages, donc c'est une musique extravagante en beaucoup de façons. ",
+        "Je l'ai créée avec une image d'elles dansant étrangement et furieusement à la fois."
+    ],
+    "12": [
+        "@",
+        "",
+        "Ceci est le thème du niveau 6.",
+        "",
+        "J'ai créé cette musique avec l'image d'un niveau très rapide avant d'atteindre le boss.",
+        "Je pense que la musique donne le sentiment d'être précipitée à toute vitesse dans le niveau, ",
+        "malgré la nature des événements qui se produisent."
+    ],
+    "13": [
+        "@",
+        "",
+        "Ceci est le thème de Okina Matara.",
+        "",
+        "Une musique du type «Ceci est le point culminant de l'histoire !» avec beaucoup de puissance.",
+        "La musique possède une mélodie puissante, même comparée aux autres musiques du jeu, donc ça aurait été bien  si j'y étais allé à fond en chantant des paroles par dessus la musique. Il est vrai que vous auriez trop ri pour jouer au jeu correctement dans ce cas, mais ça serait le meilleur résultat."
+    ],
+    "14": [
+        "@",
+        "",
+        "C'est le thème du Niveau Extra.",
+        "",
+        "Nous plongeons dans un monde mystérieux encore une fois, après avoir rassemblé notre courage.",
+        "C'est une musique à la fois étrange et douce.",
+        "",
+        "Cette musique rappelle bien la difficulté du niveau, donc c'est assez déprimant (mdr)"
+    ],
+    "15": [
+        "Éternelle et Parfaite Floraison ~ Vie Cachée Parmis les Spectres",
+        "",
+        "C'est le thème de Yuyuko Saigyouji.",
+        "",
+        "Une très gentille personne au pseudonyme de MLM3 l'a composée.",
+        "Elle reprend son premier thème dans Perfect Cherry Blossom",
+        "mais avec une instrumentation proche des Touhou plus récents.",
+        "C'est dommage, j'aurais bien voulu encore l'embêter."
+    ],
+    "16": [
+        "@",
+        "",
+        "C'est le thème de fin.",
+        "",
+        "On dirait une mauvaise fin, mais pour dire la vérité,",
+        "cette fois-ci,toutes les fins ressemblent à des mauvaises fins."
+    ],
+    "17": [
+        "@",
+        "",
+        "C'est le thème des crédits.",
+        "",
+        "C'est une version différente du thème du niveau de l'hiver créée avec l'image d'une conclusion rafraîchissante en tête.",
+        "",
+        "En parlant de ça. Pourquoi une musique du type «Blizzard de l'hiver» est rafraîchissante, me demandez-vous ?",
+        "C'est parce que je l'ai faite avec «Début du matin» en tête. «Fuyu wa tsutomete», comme ils disent."
+    ]
+}
