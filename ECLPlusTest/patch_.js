77a35d45c5af9694189e80a7ee35ac1ace71ca99
Shoxlu <shoxlu0712@gmail.com>
2022-06-21 13:42:03 +0200

    ECLPLusTest

---
+++ ECLPlusTest/patch_.js
@@ -0,0 +1,11 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"repos/exphp/bullet_cap/"
+	],
+    "id": "ShoxBazar",
+    "servers": [
+        "https://mirrors.thpatch.net/Shoxlu/ShoxBazar"
+    ],
+    "title": "idk (don't play this thing)"
+}
