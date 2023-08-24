0b8b475eac43e931e48e08294b4678febbab19db
Shoxlu <100439482+Shoxlu@users.noreply.github.com>
2023-08-24 18:28:09 +0200

    add other characters fix

--- repo.js
+++ repo.js
@@ -2,7 +2,7 @@
 	"contact": "Shoxlu#4245",
 	"id": "Shoxlu",
 	"patches": {
-		"Th19ExAttackResAnimFix": "A mod that fixes the Ex attack animation that is offset for resolutions different than 1280x960. (It happens only to a few characters such as Chiyari.)",
+		"Th19ExAttackResAnimFix": "A mod that fixes the Ex attack animation that is offset for resolutions different than 1280x960.",
 		"ModCdI": "My first mod ever, for th13 (unfinished, probably) based on some people on the Café des Illusions discord server",
 		"FestivalCreationCdi": "Un mod créé avec Lance#7161(Sprites, dialogues et autres) et MLM3#1370(Musique) à l'occasion du Festival de Créations du serveur francophone \"Le Café des Illusions\" sur le thème de Touhou 7 et du printemps !"
 	},
