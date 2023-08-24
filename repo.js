da100593f1b951ec449497eb70f3b0f6a78811e5
Shoxlu <100439482+Shoxlu@users.noreply.github.com>
2023-08-24 17:04:07 +0200

    Update repo.js

--- repo.js
+++ repo.js
@@ -3,7 +3,6 @@
 	"id": "Shoxlu",
 	"patches": {
 		"ModCdI": "My first mod ever,for th13 (unfinished, probably) based on some people on the Café des Illusions discord server",
-		"ShoxBazar": "idk (don't play this thing)",
 		"FestivalCreationCdi": "Un mod créé avec Lance#7161(Sprites, dialogues et autres) et MLM3#1370(Musique) à l'occasion du Festival de Créations du serveur francophone \"Le Café des Illusions\" sur le thème de Touhou 7 et du printemps !"
 	},
 	"servers": [
