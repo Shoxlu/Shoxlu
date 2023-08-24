2b4d24a4bb0c3774f5f16891f4826c8323c01466
Shoxlu <100439482+Shoxlu@users.noreply.github.com>
2023-08-24 16:48:26 +0200

    add a patch

---
+++ Th19ExAttackResAnimFix/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+	],
+    "id": "Th19ExAttackResAnimFix",
+    "servers": [
+        "https://mirrors.thpatch.net/Shoxlu/Th19ExAttackResAnimFix"
+    ],
+    "title": "A mod that fixes the Ex attack animation that is offset for resolutions different than 1280x960. (It happens only to a few characters such as Chiyari.)"
+}
