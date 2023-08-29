f56ec5635acc1dcaab678216c67349de61d01869
Shoxlu <100439482+Shoxlu@users.noreply.github.com>
2023-08-29 17:10:26 +0200

    push th19FixOrinLV2

---
+++ Th19FixOrinLV2/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+    "id": "Th19FixOrinLV2",
+    "servers": [
+        "https://mirrors.thpatch.net/Shoxlu/Th19FixOrinLV2"
+    ],
+    "title": "Fixes the Orin's lv2 bug, killing her when she uses it too high."
+}
