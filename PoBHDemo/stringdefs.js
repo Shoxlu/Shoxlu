dd9dcab64cf6c000de02398fb75f42314fae6f9f
Shoxlu <100439482+Shoxlu@users.noreply.github.com>
2024-04-21 08:56:08 +0200

    Update stringdefs.js

--- PoBHDemo/stringdefs.js
+++ PoBHDemo/stringdefs.js
@@ -1,6 +1,6 @@
 {
 	// "font": "Touhou Biolinum",
 	"th15 JP": "Praises of Buttered Heavens DEMO",
-	"th15": "東方塩崇拝 ~ Praises of Buttered Heavens DEMO"
+	"th15": "東方塩崇拝 ~ Praises of Buttered Heavens DEMO",
 	"th15_scorefile_fn": "scorefilePoBH.dat"
 }
