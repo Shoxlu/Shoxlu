fd9fa0ea4ec83340a8e1a324016db80ee15a8a71
Shoxlu <100439482+Shoxlu@users.noreply.github.com>
2024-07-03 10:06:25 +0200

    upload Th19BossMarker patch

---
+++ Th19BossMarker/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+    "id": "Th19BossMarker",
+    "servers": [
+        "https://mirrors.thpatch.net/Shoxlu/Th19BossMarker"
+    ],
+    "title": "Adds a boss marker like in previous games (for both player)."
+}
--- repo.js
+++ repo.js
@@ -5,6 +5,7 @@
 		"PoBHDemo": "Touhou - Praises of Buttered Heavens(DEMO). Un mod créé avec tout une bande d'amis comme Lance, Tachy, Day, Yvel, Baptoast, Melly et Lunil (et plein d'autres gens par ci par là) ! Un projet collaboratif autour du beurre et plein d'autres choses !(th15)",
 		"Th19ExAttackResAnimFix": "A mod that fixes the Ex attack animation that is offset for resolutions different than 1280x960.",
 		"Th19FixOrinLV2": "Fixes the Orin's lv2 bug, killing her when she uses it too high.",
+		"Th19BossMarker": "Adds a boss marker like in previous games (for both player).",
 		"ModCdI": "My first mod ever, for th13 (unfinished, probably) based on some people on the Café des Illusions discord server",
 		"BetterSoundClue": "A mod that changes some sound effects to help you playing the game.(th19 only)",
 		"FestivalCreationCdi": "Un mod créé avec Lance#7161(Sprites, dialogues et autres) et MLM3#1370(Musique) à l'occasion du Festival de Créations du serveur francophone \"Le Café des Illusions\" sur le thème de Touhou 7 et du printemps !"
