a8e819602a60d84194bc40dd66ff112da47a10ff
Shoxlu <100439482+Shoxlu@users.noreply.github.com>
2024-07-03 13:15:52 +0200

    Update patch.js

--- Th19FixOrinLV2/patch.js
+++ Th19FixOrinLV2/patch.js
@@ -1,10 +1,10 @@
 {
-	"dependencies": [
-		"nmlgc/base_tsa"
-	],
-    "id": "Th19FixOrinLV2",
-    "servers": [
-        "https://mirrors.thpatch.net/Shoxlu/Th19FixOrinLV2"
-    ],
-    "title": "(Th19) Fixes the Orin's lv2 bug, killing her when she uses it too high."
+  "dependencies": [
+    "nmlgc/base_tsa"
+  ],
+  "id": "Th19FixOrinLV2",
+  "servers": [
+    "https://mirrors.thpatch.net/Shoxlu/Th19FixOrinLV2/"
+  ],
+  "title": "(Th19) Fixes the Orin's lv2 bug, killing her when she uses it too high."
 }
