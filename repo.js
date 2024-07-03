bb495086c605ef24f7f644301a658b861542a533
Shoxlu <100439482+Shoxlu@users.noreply.github.com>
2024-07-03 13:15:47 +0200

    fix compatibility with balance patch

--- repo.js
+++ repo.js
@@ -1,17 +1,17 @@
 {
-	"contact": "Shoxlu#4245",
-	"id": "Shoxlu",
-	"patches": {
-		"PoBHDemo": "Touhou - Praises of Buttered Heavens(DEMO). Un mod créé avec tout une bande d'amis comme Lance, Tachy, Day, Yvel, Baptoast, Melly et Lunil (et plein d'autres gens par ci par là) ! Un projet collaboratif autour du beurre et plein d'autres choses !(th15)",
-		"Th19ExAttackResAnimFix": "(Th19) A mod that fixes the Ex attack animation that is offset for resolutions different than 1280x960.",
-		"Th19FixOrinLV2": "(Th19) Fixes the Orin's lv2 bug, killing her when she uses it too high.",
-		"Th19BossMarker": "(Th19) Adds a boss marker like in previous games (for both player).",
-		"ModCdI": "My first mod ever, for th13 (unfinished, probably) based on some people on the Café des Illusions discord server",
-		"BetterSoundClue": "A mod that changes some sound effects to help you playing the game.(th19 only)",
-		"FestivalCreationCdi": "Un mod créé avec Lance#7161(Sprites, dialogues et autres) et MLM3#1370(Musique) à l'occasion du Festival de Créations du serveur francophone \"Le Café des Illusions\" sur le thème de Touhou 7 et du printemps !"
-	},
-	"servers": [
-		"https://mirrors.thpatch.net/Shoxlu/"
-	],
-	"title": "Some projects."
+    "id": "Shoxlu",
+    "title": "Some projects.",
+    "contact": "Shoxlu#4245",
+    "servers": [
+        "https://mirrors.thpatch.net/Shoxlu/"
+    ],
+    "patches": {
+        "BetterSoundClue": "(Th19) A mod that changes some sound effects to help you playing the game.",
+        "FestivalCreationCdI": "Un mod créé avec Lance#7161(Sprites, dialogues et autres) et MLM3#1370(Musique) à l'occasion du Festival de Créations du serveur francophone \"Le Café des Illusions\" sur le thème de Touhou 7 et du printemps !",
+        "ModCdI": "My first mod ever,for th13 (unfinished, probably) based on some people on the Café des Illusions discord server",
+        "PoBHDemo": "Touhou - Praises of Buttered Heavens(DEMO). Un mod créé avec tout une bande d'amis comme Lance, Tachy, Day, Yvel, Baptoast, Melly et Lunil (et plein d'autres gens par ci par là) ! Un projet collaboratif autour du beurre et plein d'autres choses ! (th15) ",
+        "Th19BossMarker": "(Th19) Adds a boss marker like in previous games (for both player).",
+        "Th19ExAttackResAnimFix": "(Th19) A mod that fixes the Ex attack animation that is offset for resolutions different than 1280x960. (It happens only to a few characters such as Chiyari.)",
+        "Th19FixOrinLV2": "(Th19) Fixes the Orin's lv2 bug, killing her when she uses it too high."
+    }
 }
