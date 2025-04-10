14a1d8454322bbc1fc40cb1b57a925675220a963
Shoxlu <100439482+Shoxlu@users.noreply.github.com>
2025-04-10 20:55:55 +0200

    a

--- FestivalCreationCdI/patch.js
+++ FestivalCreationCdI/patch.js
@@ -8,5 +8,5 @@
     "servers": [
         "https://mirrors.thpatch.net/Shoxlu/FestivalCreationCdI"
     ],
-    "title": "Un mod créé avec Lance#7161(Sprites, dialogues et autres) et MLM3#1370(Musique) à l'occasion du Festival de Créations du serveur francophone \"Le Café des Illusions\" sur le thème de Touhou 7 et du printemps !"
+    "title": "(FestivalCreationCdI) Un mod créé avec Lance#7161(Sprites, dialogues et autres) et MLM3#1370(Musique) à l'occasion du Festival de Créations du serveur francophone \"Le Café des Illusions\" sur le thème de Touhou 7 et du printemps !"
 }
