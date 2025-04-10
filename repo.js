8b2c96635d7bba574f1967276f15114285c7c4fb
Shoxlu <100439482+Shoxlu@users.noreply.github.com>
2025-04-10 19:59:08 +0200

    Update repo.js

--- repo.js
+++ repo.js
@@ -7,7 +7,8 @@
     ],
     "patches": {
 		"PoBH": "(PoBH th15) Touhou - Praises of Buttered Heavens. Un mod créé avec tout une bande d'amis comme Lance, Tachy, Day, Yvel, Kagummi, Melly, Lunil et Utopia (et quelques interventions supplémentaires) ! Un projet collaboratif autour du beurre et plein d'autres choses ! (th15) ",
-		"FestivalCreationCdI": "Un mod créé avec Lance#7161(Sprites, dialogues et autres) et MLM3#1370(Musique) à l'occasion du Festival de Créations du serveur francophone \"Le Café des Illusions\" sur le thème de Touhou 7 et du printemps !",
+		"PoBH_en": "(PoBH_en th15) Touhou - Praises of Buttered Heavens. Translation of a mod created with a lot a friends such as Lance, Tachy, Day, Yvel, Baptoast, Melly, Lunil et Utopia (and some other friends) ! A collaborative project about butter and a lot of other things ! Contact lance_la_miko on discord for translation problem. (th15)",
+        "FestivalCreationCdI": "Un mod créé avec Lance#7161(Sprites, dialogues et autres) et MLM3#1370(Musique) à l'occasion du Festival de Créations du serveur francophone \"Le Café des Illusions\" sur le thème de Touhou 7 et du printemps !",
 		"BetterSoundClue": "(Th19) A mod that changes some sound effects to help you playing the game.",
 		"Th19BossMarker": "(Th19) Adds a boss marker like in previous games (for both player).",
         "Th19ExAttackResAnimFix": "(Th19) A mod that fixes the Ex attack animation that is offset for resolutions different than 1280x960. (It happens only to a few characters such as Chiyari.)",
