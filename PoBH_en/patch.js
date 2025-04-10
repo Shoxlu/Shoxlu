b2a76ea27b3e5b29f9c2a2f7487097a7d40f9207
Shoxlu <100439482+Shoxlu@users.noreply.github.com>
2025-04-10 19:49:19 +0200

    PoBH_en

---
+++ PoBH_en/patch.js
@@ -0,0 +1,15 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"ExpHP/bullet_cap",
+		"nmlgc/script_latin",
+		"Shoxlu/PoBH"
+	],
+    "id": "PoBH_en",
+    "servers": [
+        "https://mirrors.thpatch.net/Shoxlu/PoBH_lang_en"
+    ],
+    "title": "(PoBH Lang_en th15) Touhou - Praises of Buttered Heavens. Translation of a mod created \
+	with a lot a friends such as Lance, Tachy, Day, Yvel, Baptoast, Melly, Lunil et Utopia (and some\
+	other friends) ! A collaborative project about butter and a lot of other things !\(th15)"
+}
