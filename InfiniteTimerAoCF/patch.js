bfae61d51c483d0fc6c2446314fcf8d1129081bc
Shoxlu <100439482+Shoxlu@users.noreply.github.com>
2025-04-10 21:48:29 +0200

    Revert "a lot"
    This reverts commit 90756d248eb760d3325df64f7386064326a8a79b.

--- InfiniteTimerAoCF/patch.js
+++ InfiniteTimerAoCF/patch.js
@@ -1,11 +1,11 @@
 {
-	"dependencies": [
-		"nmlgc/base_tsa",
-		"nmlgc/base_tasofro"
-	],
-	"id": "InfiniteTimerAoCF",
-	"servers": [
-		"https://raw.githubusercontent.com/Shoxlu/Shoxlu/main/InfiniteTimerAoCF/"
-	],
-	"title": "(Th15.5) Battle timer is infinite in all modes, as in practice mode. Requested by Day."
+  "dependencies": [
+    "nmlgc/base_tsa",
+	"nmlgc/base_tasofro"
+  ],
+  "id": "AoCFInfiniteTimer",
+  "servers": [
+    "https://mirrors.thpatch.net/Shoxlu/InfiniteTimerAoCF/"
+  ],
+  "title": "(Th15.5) Battle timer is infinite in all modes, as in practice mode. Requested by Day."
 }
