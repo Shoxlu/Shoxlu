8af456a56ed3c204eb61a4e004a923bb78376a61
Shoxlu <100439482+Shoxlu@users.noreply.github.com>
2026-04-04 17:51:40 +0200

    tries with adonis

---
+++ Fixes/versions.js
@@ -0,0 +1,14 @@
+{
+	"hashes": {
+		"efaacec5041045f42660830a3c9558ec6b2cb57e5e824169aac5bfc41eb4a2de": ["adonis", "1", "(original)"],	
+		"ba2457f843c1db94dc768a6e5e56526cc188081dfcf159d54204ab8a047ed436": ["adonis", "1e", "(original)"],	
+		"ccc20365933cd3025892968b46717003091bf4c561be90c7f90cc4b388782052": ["adonis", "2", "(original)"],	
+		"340134ca47344f87e621cc58b6de0182b338b2978eb630d7895b4466a4a82fc5": ["adonis", "2e", "(original)"]
+	},
+	"sizes": {
+		"82944": ["adonis", "1", "(original)"],	
+		"82944": ["adonis", "1e", "(original)"],	
+		"96256": ["adonis", "2", "(original)"],	
+		"96256": ["adonis", "2e", "(original)"]
+	}
+}
