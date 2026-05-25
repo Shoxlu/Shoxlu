6ae81c2c049f2565fb77984b67ed6eec1d5e559f
Shoxlu <100439482+Shoxlu@users.noreply.github.com>
2026-05-26 00:11:12 +0200

    add adonis_compat and add it as Fixes dependency

---
+++ adonis_compat/patch.js
@@ -0,0 +1,10 @@
+{
+  "dependencies": [
+    "nmlgc/base_tsa"
+  ],
+  "id": "adonis_compat",
+  "servers": [
+    "https://mirrors.thpatch.net/Shoxlu/adonis_compat/"
+  ],
+  "title": "Adonis compatibility mod (likely to break)."
+}
