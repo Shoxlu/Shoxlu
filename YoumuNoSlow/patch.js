29fbbf55182e15feadfe41cdb7d3f7c3649adc1d
Shoxlu <100439482+Shoxlu@users.noreply.github.com>
2026-06-05 15:51:39 +0200

    youmuNoSlow

---
+++ YoumuNoSlow/patch.js
@@ -0,0 +1,10 @@
+{
+  "dependencies": [
+    "nmlgc/base_tsa"
+  ],
+  "id": "YoumuNoSlow",
+  "servers": [
+    "https://mirrors.thpatch.net/Shoxlu/YoumuNoSlow/"
+  ],
+  "title": "(th9) Youmu is no longer slowed during her ex attacks."
+}
---
+++ YoumuNoSlow/th09.v1.50a.js
@@ -0,0 +1,9 @@
+{
+  "binhacks": {
+	"noop_youmu_speed_decrease_ex_attack": {
+		"addr": 0x4432BE,
+		"code":"B8 0000803F"
+	}
+  }
+}
+
