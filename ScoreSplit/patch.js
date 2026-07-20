dee8eaa68ee645eec8d5e15fcc36ffdf682f74c5
Shoxlu <100439482+Shoxlu@users.noreply.github.com>
2026-07-20 13:32:21 +0200

    Create patch.js

---
+++ ScoreSplit/patch.js
@@ -0,0 +1,10 @@
+{
+  "dependencies": [
+    "nmlgc/base_tsa"
+  ],
+  "id": "ScoreSplit",
+  "servers": [
+    "https://mirrors.thpatch.net/Shoxlu/ScoreSplit/"
+  ],
+  "title": "(th16) (beta) Make splits for recording scores."
+}
